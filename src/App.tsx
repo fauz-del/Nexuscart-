45a142b228c7f2f966f18764d32ad3dd848a4d52
fauz-del <fauzeeyatagbalaya27@gmail.com>
2026-01-12 00:11:55 +0100

    Initial NexusCart setup with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-green-400">
+        NexusCart is ready 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
